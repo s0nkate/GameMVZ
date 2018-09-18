bd85318e0c40ee8cb1097f9bc2b783a0f9bec4a1
huydv8297 <huydv8297@gmail.com>
2018-09-18 14:45:31 +0700

    xong popup shop

---
+++ Manga vs Zombie/Assets/Scripts/ShopManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopManager : MonoBehaviour {
+
+	public GameObject tab1;
+	public GameObject tab2;
+	public GameObject tab3;
+	void Start () {
+		tab1 = GetComponent<GameObject>();
+		tab2 = GetComponent<GameObject>();
+		tab3 = GetComponent<GameObject>();
+
+		tab1.SetActive(true);
+		tab1.SetActive(false);
+		tab1.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
