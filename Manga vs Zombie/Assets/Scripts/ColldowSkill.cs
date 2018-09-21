1da620e4434313dc198899ca4e2b7f5d7c4aea0a
s0nkate <42999081+s0nkate@users.noreply.github.com>
2018-09-21 17:26:42 +0700

    Hành động nhân vật

---
+++ Manga vs Zombie/Assets/Scripts/ColldowSkill.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColldowSkill : MonoBehaviour {
+
+    public Image imageColldown;
+    public float cooldown = 5;
+    bool isCooldown;
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            isCooldown = true;
+        }
+        if (isCooldown)
+        {
+            imageColldown.fillAmount += 1 / cooldown * Time.deltaTime;
+        }
+        if(imageColldown.fillAmount >= 1)
+        {
+            imageColldown.fillAmount = 0;
+            isCooldown = false;
+        }
+	}
+}
--- Manga vs Zombie/Assets/Scripts/Player1Controller.cs
+++ Manga vs Zombie/Assets/Scripts/Player1Controller.cs
@@ -1,26 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Player1Controller : MonoBehaviour {
+public class Player1Controller : MonoBehaviour
+{
 
     public bool faceright = true;
-    public float attackdelay = 0.3f;
+    public float attackdelay = 0.4f;
     public bool attacking = false;
     public bool attacking1 = false;
     public Animator anim;
     public Collider2D trigger;
-   
+    public bool skill1 = false;
+    public float skill1delay = 1;
+    public bool skill2 = false;
+    public float skill2delay = 0.5f;
+    public Collider2D trigger1;
+    public Collider2D trigger2;
+    public Image imageColldown1;
+    public float cooldown1 = 5;
+    public bool isCooldown1=false;
+    public Image imageColldown2;
+    public float cooldown2 = 0.5f;
+    public bool isCooldown2 = false;
     void Awake()
     {
         anim = gameObject.GetComponent<Animator>();
         trigger.enabled = false;
-        
+
     }
 
-    
-    void Update () {
-        float h = Input.GetAxis("Horizontal");
+
+    void FixedUpdate()
+    {
+        float h = 0;
         if (h > 0 && !faceright)
         {
             Flip();
@@ -31,20 +45,9 @@ public class Player1Controller : MonoBehaviour {
         {
             Flip();
 
+        }
 
-        }
-        if (Input.GetKeyDown(KeyCode.LeftArrow)&& !attacking)
-        {
-            Attack();
-            trigger.enabled = true;
-            attackdelay = 0.3f;
-        }
-        if (Input.GetKeyDown(KeyCode.RightArrow) && !attacking)
-        {
-            Attack();
-            trigger.enabled = true;
-            attackdelay = 0.3f;
-        }
+
         if (attacking1)
         {
             if (attackdelay > 0)
@@ -53,10 +56,11 @@ public class Player1Controller : MonoBehaviour {
             }
             else
             {
-             
+
                 attacking1 = false;
+
                 trigger.enabled = false;
-                
+
             }
         }
         if (attacking)
@@ -73,8 +77,61 @@ public class Player1Controller : MonoBehaviour {
 
             }
         }
+        if (skill1)
+        {
+             
+            if (skill1delay > 0)
+            {
+                skill1delay -= Time.deltaTime;
+            }
+            else
+            {
+
+                skill1 = false;
+                trigger1.enabled = false;
+
+            }
+        }
+       
+        if (isCooldown1)
+            {
+                imageColldown1.fillAmount += Time.deltaTime;
+            }
+            if (imageColldown1.fillAmount >= 1)
+            {
+                imageColldown1.fillAmount = 0;
+                isCooldown1 = false;
+            }
+        if (isCooldown2)
+        {
+            imageColldown2.fillAmount += cooldown2*Time.deltaTime;
+        }
+        if (imageColldown2.fillAmount >= 1)
+        {
+            imageColldown2.fillAmount = 0;
+            isCooldown2 = false;
+        }
+        if (skill2)
+        {
+            if (skill2delay > 0)
+            {
+                skill2delay -= Time.deltaTime;
+            }
+            else
+            {
+
+                skill2 = false;
+                trigger2.enabled = false;
+
+            }
+        }
+        
+
+      
         anim.SetBool("Attacking", attacking);
         anim.SetBool("Attacking1", attacking1);
+        anim.SetBool("Skill1", skill1);
+        anim.SetBool("Skill2", skill2);
 
 
     }
@@ -82,22 +139,81 @@ public class Player1Controller : MonoBehaviour {
     {
         faceright = !faceright;
         Vector3 Scale;
-        Scale= transform.localScale;
+        Scale = transform.localScale;
         Scale.x *= -1;
         transform.localScale = Scale;
     }
     public void Attack()
     {
         int random = Random.Range(0, 2);
-        if(random == 0)
+        if (random == 0)
         {
             attacking = true;
         }
-        else 
+        else
         {
             attacking1 = true;
         }
 
 
     }
+    public void AttackLeft()
+    {
+        float h = -1;
+        if (h > 0 && !faceright)
+        {
+            Flip();
+
+
+        }
+        if (h < 0 && faceright)
+        {
+            Flip();
+
+
+        }
+        Attack();
+        trigger.enabled = true;
+        attackdelay = 0.3f;
+    }
+    public void AttackRight()
+    {
+        float h = 1;
+        if (h > 0 && !faceright)
+        {
+            Flip();
+
+
+        }
+        if (h < 0 && faceright)
+        {
+            Flip();
+
+
+        }
+        Attack();
+        trigger.enabled = true;
+        attackdelay = 0.3f;
+    }
+    public void Skill1()
+    {
+        isCooldown1 = true;
+        skill1 = true;
+        skill1delay = 1;
+        trigger1.enabled = true;
+        
+        
+
+    }
+    public void Skill2()
+    {
+        isCooldown2 = true;
+        skill2 = true;
+        skill2delay = 0.5f;
+        trigger2.enabled = true;
+
+    }
+   
 }
+    
+
