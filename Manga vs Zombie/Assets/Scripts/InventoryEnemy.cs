6e95efc31664c109c4e7cf529c7b6d3b8ba33174
s0nkate <42999081+s0nkate@users.noreply.github.com>
2018-10-18 13:58:56 +0700

    editor part2

---
+++ Manga vs Zombie/Assets/Scripts/InventoryEnemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+
+using UnityEngine;
+[System.Serializable]
+public class InventoryEnemy  {
+    public string name;
+    public Texture2D image = null;
+    public float health;
+    public float speed;
+    public float damage;
+    public float Delay;
+    public int money;
+    public int score;
+    public enum Type {Walker=0
+            ,Runner=1
+            ,Hulker=2,
+             Exploder=3 };
+    public Type type; 
+    public AnimationClip idle;
+    public AnimationClip walk;
+    public AnimationClip attack;
+    public AnimationClip dead;
+}
