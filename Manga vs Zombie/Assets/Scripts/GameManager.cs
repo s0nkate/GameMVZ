cf7b0fe4322bbc2ee6e8ad2d50d6f2501697b45c
s0nkate <42999081+s0nkate@users.noreply.github.com>
2018-10-25 14:10:11 +0700

    complete!

--- Manga vs Zombie/Assets/Scripts/GameManager.cs
+++ Manga vs Zombie/Assets/Scripts/GameManager.cs
@@ -13,6 +13,9 @@ public class GameManager : MonoBehaviour {
     public Text timeText;
     public Text goldText;
     public Text HighScoreText;
+    public Text ScoreWin;
+    public Text GoldWin;
+
     public InventorySceneList scenelist;
     public Texture2D Backgournd;
     public Texture2D Foregournd;
@@ -20,20 +23,35 @@ public class GameManager : MonoBehaviour {
     public Texture2D Towerenemy;
     public int i=0;
     float t=0;
-    private bool pause = false;
+    private bool pause = true;
     public GameObject PauseUI;
     public GameObject ResultUI;
+    public GameObject NextLvUI;
+    public GameObject MenuUI;
+    public GameObject YNUI;
+    public GameObject Player;
+    public GameObject Highscore;
+    public GameObject YNQuitUI;
+    public GameObject start;
+    public GameObject shop;
+    public GameObject setting;
+    public GameObject quit;
+    public GameObject quitResult;
+    public GameObject backResult;
+    public GameObject Pausebtn;
     private bool endgame = false;
     void Start () {
+
+        YNUI.SetActive(false);
+        Player.SetActive(false);
+        MenuUI.SetActive(true);
         PauseUI.SetActive(false);
         ResultUI.SetActive(false);
-        time = scenelist.scenelist[i].TimePlay;
-        Score = 0;
-        Gold = 0;
-        Backgournd = scenelist.scenelist[i].Backgournd;
-        Foregournd = scenelist.scenelist[i].Foregournd;
-        Tower = scenelist.scenelist[i].Tower;
-        Towerenemy = scenelist.scenelist[i].Towerenemy;
+        NextLvUI.SetActive(false);
+        Highscore.SetActive(false);
+        YNQuitUI.SetActive(false);
+        HighScore = PlayerPrefs.GetInt("HighScore");
+        HighScoreText.text = HighScore.ToString();
         
     
     }
@@ -58,7 +76,7 @@ public class GameManager : MonoBehaviour {
 
         if (time==0 && endgame==false && i< scenelist.scenelist.Count-1)
         {
-           
+            NextLv();
             UpLevel();
             LoadLevel();
         }
@@ -87,9 +105,7 @@ public class GameManager : MonoBehaviour {
     }
     public void LoadLevel()
     {
-
-
-        
+  
             i++;
         time = scenelist.scenelist[i].TimePlay;
         Backgournd = scenelist.scenelist[i].Backgournd;
@@ -105,26 +121,139 @@ public class GameManager : MonoBehaviour {
         ResultUI.SetActive(true);
         endgame = true;
         pause = true;
-        
+
+        ScoreWin.text = Score.ToString();
+        GoldWin.text = "+" + Gold.ToString();
         HighScore = PlayerPrefs.GetInt("HighScore");
         if (HighScore < Score)
         {
             PlayerPrefs.SetInt("HighScore", Score);
+            Highscore.SetActive(true);
         }
         Debug.Log(HighScore);
     }
     public void Pause()
     {
-        PauseUI.SetActive(true);
-        pause = true;
+        if (endgame == false && NextLvUI.activeInHierarchy==false)
+        {
+            PauseUI.SetActive(true);
+            pause = true;
+        }
     }
     public void Resume()
     {
         pause = false;
         PauseUI.SetActive(false);
     }
-    public void Exit()
+   
+    public void NextLv()
+    {
+        pause = true;
+        NextLvUI.SetActive(true);
+
+    }
+    public void NextLV()
+    {
+        pause = false;
+        NextLvUI.SetActive(false);
+    }
+    public void PlayGame()
+    {
+        Player.SetActive(true);
+        MenuUI.SetActive(false);
+        ResultUI.SetActive(false);
+        NextLvUI.SetActive(false);
+        quitResult.SetActive(true);
+        backResult.SetActive(true);
+        Pausebtn.SetActive(true);
+
+        pause = false;
+        endgame = false;
+        time = scenelist.scenelist[0].TimePlay;
+        Score = 0;
+        Gold = 0;
+        Backgournd = scenelist.scenelist[0].Backgournd;
+        Foregournd = scenelist.scenelist[0].Foregournd;
+        Tower = scenelist.scenelist[0].Tower;
+        Towerenemy = scenelist.scenelist[0].Towerenemy;
+    }
+    public void BackMenu()
+    {
+        PauseUI.SetActive(false);
+        YNUI.SetActive(true);
+        quitResult.SetActive(false);
+        backResult.SetActive(false);
+        Pausebtn.SetActive(false);
+    }
+    public void Yesbtn()
+    {
+        
+        i = 0;
+        YNUI.SetActive(false);
+        MenuUI.SetActive(true);
+        endgame = true;
+        Player.SetActive(false);
+        Highscore.SetActive(false);
+      
+        HighScoreText.text = HighScore.ToString();
+        
+     
+    }
+    public void Nobtn()
+    {
+        Pausebtn.SetActive(true);
+        quitResult.SetActive(true);
+        backResult.SetActive(true);
+        YNUI.SetActive(false);
+        pause = false;
+    }
+    public void QuitGame()
+    {
+        YNQuitUI.SetActive(true);
+        Pausebtn.SetActive(false);
+        if (PauseUI.activeInHierarchy == true)
+            { 
+            PauseUI.SetActive(false);
+        }
+        if (ResultUI.activeInHierarchy == true)
+        {
+            quitResult.SetActive(false);
+            backResult.SetActive(false);
+        }
+        if (MenuUI.activeInHierarchy == true)
+        {
+            start.SetActive(false);
+            shop.SetActive(false);
+            setting.SetActive(false);
+            quit.SetActive(false);
+        }
+    }
+    public void YesQbtn()
     {
         Application.Quit();
+
+    }
+    public void NoQbtn()
+    {
+        YNQuitUI.SetActive(false);
+        Pausebtn.SetActive(true);
+        if (MenuUI.activeInHierarchy == true)
+        {
+            start.SetActive(true);
+            shop.SetActive(true);
+            setting.SetActive(true);
+            quit.SetActive(true);
+        }
+        if (PauseUI.activeInHierarchy == false)
+        {
+            pause = false;
+        }
+        if (ResultUI.activeInHierarchy == true)
+        {
+            quitResult.SetActive(true);
+            backResult.SetActive(true);
+        }
+
+
     }
 }
