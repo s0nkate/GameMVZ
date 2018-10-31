b3c2be214a2055216adfbb38ee92b63ed58825db
huydv8297 <huydv8297@gmail.com>
2018-10-31 09:21:48 +0700

    sua animation

--- Manga vs Zombie/Assets/Scripts/Components/Player.cs
+++ Manga vs Zombie/Assets/Scripts/Components/Player.cs
@@ -38,18 +38,18 @@ namespace ECSComponent
 				bool isActive = gameObject.activeSelf;
 				Vector3 position = transform.position;
 				Quaternion rotation = transform.rotation;
-				bool skill1 = animator.GetBool("Skill1");
-				bool skill2 = animator.GetBool("Skill2");
-				bool actack1 = animator.GetBool("Attacking");
-				bool actack2 = animator.GetBool("Attacking1");
+				// bool skill1 = animator.GetBool("Skill1");
+				// bool skill2 = animator.GetBool("Skill2");
+				// bool actack1 = animator.GetBool("Attacking");
+				// bool actack2 = animator.GetBool("Attacking1");
 
 				stream.Serialize(ref isActive);
 				stream.Serialize(ref position);
 				stream.Serialize(ref rotation);
-				stream.Serialize(ref skill1);
-				stream.Serialize(ref skill2);
-				stream.Serialize(ref actack1);
-				stream.Serialize(ref actack2);
+				// stream.Serialize(ref skill1);
+				// stream.Serialize(ref skill2);
+				// stream.Serialize(ref actack1);
+				// stream.Serialize(ref actack2);
 				Debug.Log("send");
       	  	}
       	 	else
@@ -66,30 +66,30 @@ namespace ECSComponent
 				bool isActive = gameObject.activeSelf;
 				Vector3 position = transform.position;
 				Quaternion rotation = transform.rotation;
-				bool skill1 = animator.GetBool("Skill1");
-				bool skill2 = animator.GetBool("Skill2");
-				bool actack1 = animator.GetBool("Attacking");
-				bool actack2 = animator.GetBool("Attacking1");
+				// bool skill1 = animator.GetBool("Skill1");
+				// bool skill2 = animator.GetBool("Skill2");
+				// bool actack1 = animator.GetBool("Attacking");
+				// bool actack2 = animator.GetBool("Attacking1");
 
 				//photonView.RequestOwnership();
 
 				stream.Serialize(ref isActive);
 				stream.Serialize(ref position);
 				stream.Serialize(ref rotation);
-				stream.Serialize(ref skill1);
-				stream.Serialize(ref skill2);
-				stream.Serialize(ref actack1);
-				stream.Serialize(ref actack2);
+				// stream.Serialize(ref skill1);
+				// stream.Serialize(ref skill2);
+				// stream.Serialize(ref actack1);
+				// stream.Serialize(ref actack2);
 				// if(!photonView.isMine)
 				// {
 					gameObject.SetActive(isActive);
 					transform.position = position;
 					transform.rotation = rotation;
-					animator.SetBool("Skill1", (bool) skill1);
-        			animator.SetBool("Skill2", (bool) skill2);
-        			animator.SetBool("Attacking", (bool) actack1);
-        			animator.SetBool("Attacking1", (bool) actack2);
-					Debug.Log("receive " + actack1 + actack2 + skill1 + skill2);
+					// animator.SetBool("Skill1", (bool) skill1);
+        			// animator.SetBool("Skill2", (bool) skill2);
+        			// animator.SetBool("Attacking", (bool) actack1);
+        			// animator.SetBool("Attacking1", (bool) actack2);
+					// Debug.Log("receive " + actack1 + actack2 + skill1 + skill2);
 				// }
        		}
 		}
