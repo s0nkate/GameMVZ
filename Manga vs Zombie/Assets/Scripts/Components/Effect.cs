0bbf13467fafc32efadbd6962eeabc3fc0ec2bf7
huydv8297 <huydv8297@gmail.com>
2018-11-14 09:09:01 +0700

    fix effect

--- Manga vs Zombie/Assets/Scripts/Components/Effect.cs
+++ Manga vs Zombie/Assets/Scripts/Components/Effect.cs
@@ -1,101 +1,90 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System;
 
-
-
-namespace ECSComponent
-{
-public enum EffectType : int 
-{
-	None = -1,
-	HeathUp = 0,
-	DamageDown = 1,
-	HouseDeffent = 2
-}
-
-public class Effect : MonoBehaviour 
-{
-    public Animator anim;
-    public bool shield;
-	public const int hpUpValue = 100;
-	public const int damageDownValue =10;
-	public const float timeEffect = 5;
-	public float t = 0;
-    public GameObject arrow;
-	Heath heath;
-	Attack attack;
-	Zombie zombie;
-
-	void Awake()
-	{
-		heath = GetComponent<Heath>();
-		attack = GetComponent<Attack>();
-		zombie = GetComponent<Zombie>();
-        anim =  GetComponent<Animator>();
-	}
-
-	[PunRPC]
-	void HPUp()
-	{
-            anim.SetTrigger("trigger");
-        
-		if(heath == null)
-			return;
-		int newHP = heath.value + hpUpValue;
-		heath.value = newHP > heath.maxValue ? heath.maxValue : newHP;	
-		GameManager.Instance.effectType = EffectType.None;
-     
+namespace ECSComponent {
+    public enum EffectType : int {
+        None = -1,
+        HeathUp = 0,
+        DamageDown = 1,
+        HouseDeffent = 2
     }
 
-	[PunRPC]
-	void DamageDown()
-	{
-            attack.damage = Math.Abs(zombie.tempDamage - damageDownValue);
-            arrow.SetActive(true);
-            Invoke("CleanEffect", timeEffect);
-    }
+    public class Effect : MonoBehaviour {
+        public Animator anim;
+        public bool shield;
+        public const int hpUpValue = 100;
+        public const int damageDownValue = 10;
+        public const float timeEffect = 5;
+        public float t = 0;
+        bool isCalledClean;
+        public GameObject arrow;
+        Heath heath;
+        Attack attack;
+        Zombie zombie;
 
-	[PunRPC]
-	void HouseDeffent()
-	{
-		if(zombie == null)
-        {
-                if (!shield)
-                {
-                    shield = true;
-                    anim.SetBool("bool", shield);
-                }
-
-        }else
-            {
-                
-                attack.damage = 0;
-                
-                
-            }
-        Invoke("CleanEffect", timeEffect);  
+        void Awake () {
+            heath = GetComponent<Heath> ();
+            attack = GetComponent<Attack> ();
+            zombie = GetComponent<Zombie> ();
+            anim = GetComponent<Animator> ();
         }
 
-	void CleanEffect()
-	{
-            shield = false;
-            
-            if (zombie == null)
+        [PunRPC]
+        void HPUp () {
+            anim.SetTrigger ("trigger");
+
+            if (heath == null)
+                return;
+            int newHP = heath.value + hpUpValue;
+            heath.value = newHP > heath.maxValue ? heath.maxValue : newHP;
+            GameManager.Instance.effectType = EffectType.None;
+
+        }
+
+        [PunRPC]
+        void DamageDown () {
+            int newDamage = zombie.tempDamage - damageDownValue;
+            attack.damage = newDamage > 0 ? newDamage : 0;
+            arrow.SetActive (true);
+            if(!isCalledClean)
             {
-                
-                anim.SetBool("bool", false);
-                GameManager.Instance.effectType = EffectType.None;
+                isCalledClean = true;
+                Invoke ("CleanEffect", timeEffect);
             }
-            else
+        }
+
+        [PunRPC]
+        void HouseDeffent () {
+            if (zombie == null) {
+                if (!shield) {
+                    shield = true;
+                    anim.SetBool ("bool", shield);
+                }
+            } else {
+                attack.damage = 0;
+            }
+            if(!isCalledClean)
             {
-                arrow.SetActive(false);
+                isCalledClean = true;
+                Invoke ("CleanEffect", timeEffect);
+            }
+            
+        }
+
+        void CleanEffect () {
+            shield = false;
+            if (zombie == null) {
+                anim.SetBool ("bool", false);
+                GameManager.Instance.effectType = EffectType.None;
+            } else {
+                arrow.SetActive (false);
                 GameManager.Instance.effectType = EffectType.None;
                 attack.damage = zombie.tempDamage;
             }
-            
+
         }
 
-}
+    }
 }
